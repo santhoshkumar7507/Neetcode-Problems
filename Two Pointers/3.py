c512d1cade7f94ef6c39b952566f8687bf22ec68
santhoshkumar7507 <ssksanthoshkumar1433@gmail.com>
2026-05-12 10:59:36 +0530

    3Sum

---
+++ Two Pointers/3.py
@@ -0,0 +1,23 @@
+# # 3Sum
+# Input: nums = [-1,0,1,2,-1,-4]
+
+# Output: [[-1,-1,2],[-1,0,1]]
+
+# Explanation:
+# nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+# nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+# nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+# The distinct triplets are [-1,0,1] and [-1,-1,2].
+
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        tmp = [nums[i], nums[j], nums[k]]
+                        res.add(tuple(tmp))
+        return [list(i) for i in res]
