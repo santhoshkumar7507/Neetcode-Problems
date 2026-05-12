88596b301d082966aa371865e7a8dc0470997996
santhoshkumar7507 <ssksanthoshkumar1433@gmail.com>
2026-05-12 10:30:15 +0530

    Valid Anagram

---
+++ Arrays & Hashing/2.py
@@ -0,0 +1,11 @@
+# Valid Anagram
+
+# Input: s = "racecar", t = "carrace"
+# Output: true
+from typing import List
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+
+    
