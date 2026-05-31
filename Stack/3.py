2afd60d348326ce292a062f64a7d0687249bf2d6
santhoshkumar7507 <ssksanthoshkumar1433@gmail.com>
2026-05-31 09:48:16 +0530

    Daily Temperatures

---
+++ Stack/3.py
@@ -0,0 +1,28 @@
+# Daily Temperatures
+
+# You are given an array of integers temperatures where temperatures[i] represents the daily temperatures on the ith day.
+
+# Return an array result where result[i] is the number of days after the ith day before a warmer temperature appears on a future day. If there is no day in the future where a warmer temperature will appear for the ith day, set result[i] to 0 instead.
+
+# Example 1:
+
+# Input: temperatures = [30,38,30,36,35,40,28]
+
+# Output: [1,4,1,2,1,0,0]
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        res = []
+
+        for i in range(n):
+            count = 1
+            j = i + 1
+            while j < n:
+                if temperatures[j] > temperatures[i]:
+                    break
+                j += 1
+                count += 1
+            count = 0 if j == n else count
+            res.append(count)
+        return res
