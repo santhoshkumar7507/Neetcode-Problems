a9a2247f88be95b218041373c5904ef2c1c8ad3a
santhoshkumar7507 <ssksanthoshkumar1433@gmail.com>
2026-06-01 12:16:06 +0530

    Answers Implement

--- Binary Search/2.py
+++ Binary Search/2.py
@@ -1,17 +1,25 @@
 
 
-You are given an m x n 2-D integer array matrix and an integer target.
+# You are given an m x n 2-D integer array matrix and an integer target.
 
-Each row in matrix is sorted in non-decreasing order.
-The first integer of every row is greater than the last integer of the previous row.
-Return true if target exists within matrix or false otherwise.
+# Each row in matrix is sorted in non-decreasing order.
+# The first integer of every row is greater than the last integer of the previous row.
+# Return true if target exists within matrix or false otherwise.
 
-Can you write a solution that runs in O(log(m * n)) time?
+# Can you write a solution that runs in O(log(m * n)) time?
 
-Example 1:
+# Example 1:
 
 
 
-Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 10
+# Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 10
 
-Output: true
+# Output: true
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == target:
+                    return True
+        return False
