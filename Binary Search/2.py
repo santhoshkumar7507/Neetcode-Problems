3dc7e29e94d76d694828db58365f30e26e55be1b
santhoshkumar7507 <ssksanthoshkumar1433@gmail.com>
2026-06-01 12:15:06 +0530

    Questions implement

---
+++ Binary Search/2.py
@@ -0,0 +1,17 @@
+
+
+You are given an m x n 2-D integer array matrix and an integer target.
+
+Each row in matrix is sorted in non-decreasing order.
+The first integer of every row is greater than the last integer of the previous row.
+Return true if target exists within matrix or false otherwise.
+
+Can you write a solution that runs in O(log(m * n)) time?
+
+Example 1:
+
+
+
+Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 10
+
+Output: true
