e60fcd5a003ad29b381d32f60278640d78d3d2ce
santhoshkumar7507 <ssksanthoshkumar1433@gmail.com>
2026-06-01 12:18:37 +0530

    Answer Implement

--- Binary Search/3.py
+++ Binary Search/3.py
@@ -11,3 +11,21 @@
 # Input: piles = [1,4,3,2], h = 9
 
 # Output: 2
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
+
+        while l <= r:
+            k = (l + r) // 2
+
+            totalTime = 0
+            for p in piles:
+                totalTime += math.ceil(float(p) / k)
+            if totalTime <= h:
+                res = k
+                r = k - 1
+            else:
+                l = k + 1
+        return res
